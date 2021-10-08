c0e1c19ce249c38279a07a9dba9608897c81122c
Jfisher37 <joseph.eric.fisher@gmail.com>
2021-10-07 22:14:31 -0400

    set up html routes and api get route.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const db = require('./db/db.json');
 const express = require ('express');
+const path = require ('path');
 
 const app = express(); 
 
@@ -15,10 +16,16 @@ app.use(express.static('public'));
 
 // set up get routes for HTMLs
 
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
 // set up API routes
+app.get('/api/notes', (req, res) => {fs.readFile('./db/db.json', (err, data) =>
+res.send(data))}); 
 
+
+app.post('/api/notes', (req, res) =>)
 
 // set up listeners
 app.listen(PORT, () =>
