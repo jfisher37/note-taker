cb3dcedd4630f8373ea5ece17099196a05d139ea
Jfisher37 <joseph.eric.fisher@gmail.com>
2021-10-07 21:55:22 -0400

    set up middleware, PORT, and listener.

--- server.js
+++ server.js
@@ -3,3 +3,23 @@ const db = require('./db/db.json');
 const express = require ('express');
 
 const app = express(); 
+
+//set up PORT and middleware
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+
+// set up get routes for HTMLs
+
+
+
+// set up API routes
+
+
+// set up listeners
+app.listen(PORT, () =>
+console.info(`listening in on PORT ${PORT}`));
